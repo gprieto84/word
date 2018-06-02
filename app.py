36335f2c24881d37f8de2e36a8f03449d7e7b8e2
german <german.prt@gmail.com>
2018-06-02 18:08:31 -0500

    Database

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
