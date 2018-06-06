9f7ca4a00e4ce8e42c810c196e08b1fad8807f3b
german <german.prt@gmail.com>
2018-06-06 05:24:33 -0500

    Style and Spinner

--- static/main.js
+++ static/main.js
@@ -1,3 +1,4 @@
+
 (function () {
   'use strict';
 
@@ -5,6 +6,10 @@
 
   .controller('WordcountController', ['$scope', '$log', '$http','$timeout',
   function($scope, $log, $http,$timeout) {
+
+  $scope.submitButtonText = 'Submit';
+  $scope.loading = false;
+  $scope.urlerror = false;
 
   $scope.getResults = function() {
 
@@ -14,14 +19,18 @@
     var userInput = $scope.url;
 
     // fire the API request
-    $http.post('/start', {"url": userInput}).
-      success(function(results) {
-        $log.log(results);
-        getWordCount(results);
-      }).
-      error(function(error) {
-        $log.log(error);
-      });
+    $http.post('/start', {'url': userInput}).
+    success(function(results) {
+      $log.log(results);
+      getWordCount(results);
+      $scope.wordcounts = null;
+      $scope.loading = true;
+      $scope.submitButtonText = 'Loading...';
+      $scope.urlerror = false;
+    }).
+    error(function(error) {
+      $log.log(error);
+    });
 
   }
   
@@ -35,6 +44,8 @@
             $log.log(data, status);
           } else if (status === 200){
             $log.log(data);
+            $scope.loading = false;
+            $scope.submitButtonText = "Submit";
             $scope.wordcounts = data;
             $timeout.cancel(timeout);
             return false;
@@ -42,6 +53,12 @@
           // continue to call the poller() function every 2 seconds
           // until the timeout is cancelled
           timeout = $timeout(poller, 2000);
+        }).
+        error(function(error) {
+          $log.log(error);
+          $scope.loading = false;
+          $scope.submitButtonText = "Submit";
+          $scope.urlerror = true;
         });
     };
     poller();
